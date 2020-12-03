6ef512364cec608643d136796844ab6e6ddd4d4d
Tom Bartels <tbartels@flexport.com>
2020-12-03 08:59:08 +0100

    Solved assignment 1 part 1 and 2

--- src/main/scala/assignments/Assignment1.scala
+++ src/main/scala/assignments/Assignment1.scala
@@ -1,5 +1,46 @@
 package assignments
 
 object Assignment1 {
-  def isNewFile = false
+
+  implicit class Crossable[X](xs: Traversable[X]) {
+    def cross[Y](ys: Traversable[Y]) =
+      for { x <- xs; y <- ys } yield (x, y)
+  }
+
+  implicit def flatten[A, B, C](t: ((A, B), C)): (A, B, C) =
+    (t._1._1, t._1._2, t._2)
+
+  def part1(input: List[Int]): Int = {
+    val combination =
+      input
+        .cross(input)
+        .find(sumTuple2Equals(2020))
+
+    combination match {
+      case Some((val1, val2)) => val1 * val2
+      case None               => 0
+    }
+  }
+
+  def part2(input: List[Int]): Int = {
+    val combination =
+      input
+        .cross(input)
+        .cross(input)
+        .map(flatten)
+        .find(sumTuple3Equals(2020))
+
+    combination match {
+      case Some((val1, val2, val3)) => val1 * val2 * val3
+      case None                     => 0
+    }
+  }
+
+  def sumTuple2Equals(value: Int) = { (tuple: (Int, Int)) =>
+    (tuple._1 + tuple._2) == value
+  }
+
+  def sumTuple3Equals(value: Int) = { (tuple: (Int, Int, Int)) =>
+    (tuple._1 + tuple._2 + tuple._3) == value
+  }
 }
