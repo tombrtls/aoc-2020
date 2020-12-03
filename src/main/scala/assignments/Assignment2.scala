d5b428ca27b6661024df103977c904f2e7bcfe8f
Tom Bartels <tbartels@flexport.com>
2020-12-03 09:01:53 +0100

    Set up boilerplate for assignment 2

---
+++ src/main/scala/assignments/Assignment2.scala
@@ -0,0 +1,7 @@
+package assignments
+
+object Assignment2 {
+  def part1(input: List[Int]): Int = ???
+
+  def part2(input: List[Int]): Int = ???
+}
---
+++ src/test/scala/assignments/Assignment2Spec.scala
@@ -0,0 +1,16 @@
+package assignments
+
+import org.scalatest._;
+import flatspec._;
+import matchers._;
+
+class Assignment1Spec extends AnyFlatSpec with should.Matchers {
+  var testInput = ???
+  var actualInput = ???
+
+  "part 1" should "do something" in {}
+  "part 1" should "do something" in {}
+
+  "part 2" should "do something" in {}
+  "part 2" should "do something" in {}
+}
