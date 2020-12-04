262bd8f2ca00e32cb5e3557b7f10c7ef582fcbdc
Tom Bartels <tbartels@flexport.com>
2020-12-04 01:57:12 +0100

    Finished Assignment 2

--- src/main/scala/aoc2020/assignments/Assignment2.scala
+++ src/main/scala/aoc2020/assignments/Assignment2.scala
@@ -1,7 +1,38 @@
 package aoc2020.assignments
 
-object Assignment2 {
-  def part1(input: List[Int]): Int = ???
+import aoc2020.Helper
 
-  def part2(input: List[Int]): Int = ???
+import scala.util.Try
+
+case class Password(range: Range, char: Char, password: String) {
+  def isValid: Boolean = {
+    range.inclusive.contains(
+      password.count(_ == char)
+    )
+  }
+
+  def firstChar: Option[Char] = Try(password.charAt(range.start - 1)).toOption
+  def secondChar: Option[Char] = Try(password.charAt(range.end - 1)).toOption
+
+  def isValidPart2: Boolean = {
+    (firstChar.contains(char) || secondChar.contains(char)) && (firstChar != secondChar)
+  }
+}
+
+object Assignment2 extends BaseAssignment [List[Password], Int]{
+  override def prepareInput(filePath: String) = {
+    val passwordRegex = """(\d+)-(\d+) (\w): (\w*)""".r
+    Helper.readLinesFromFile(filePath)
+      .map {
+        case passwordRegex(min, max, char, password) => Password(Range(min.toInt, max.toInt), char.charAt(0), password)
+      }
+      .toList
+  }
+
+  override def processPart1(input: List[Password]) = {
+    input.count(_.isValid)
+  }
+
+  override def processPart2(input: List[Password]) =
+    input.count(_.isValidPart2)
 }
--- src/test/scala/aoc2020/Assignment2Spec.scala
+++ src/test/scala/aoc2020/Assignment2Spec.scala
@@ -5,12 +5,44 @@ import flatspec._;
 import matchers._;
 
 class Assignment2Spec extends AnyFlatSpec with should.Matchers {
-  var testInput = ???
-  var actualInput = ???
+  var testInput = "assign_2_test.txt"
+  var actualInput = "assign_2.txt"
 
-  "part 1" should "do something" in {}
-  "part 1" should "do something" in {}
+  "Password.isValid" should "return false" in {
+    Password(Range(2, 4), 'a', "a").isValid should be(false)
+    Password(Range(2, 4), 'a', "aaaaa").isValid should be (false)
+  }
 
-  "part 2" should "do something" in {}
-  "part 2" should "do something" in {}
+  "Password.isValid" should "return true" in {
+    Password(Range(1, 3), 'a', "a").isValid should be(true)
+    Password(Range(1, 3), 'a', "aa").isValid should be(true)
+    Password(Range(1, 3), 'a', "aaa").isValid should be (true)
+  }
+
+  "Password.isValidPart2" should "return true" in {
+    Password(Range(1, 3), 'a', "aab").isValidPart2 should be(true)
+    Password(Range(1, 3), 'a', "baa").isValidPart2 should be(true)
+    Password(Range(1, 3), 'a', "a").isValidPart2 should be(true)
+  }
+
+  "Password.isValidPart2" should "return false" in {
+    Password(Range(1, 3), 'a', "aaa").isValidPart2 should be(false)
+    Password(Range(1, 3), 'a', "bbb").isValidPart2 should be(false)
+    Password(Range(1, 3), 'a', "aba").isValidPart2 should be(false)
+  }
+
+  "part 1" should "return 2" in {
+    Assignment2.executePart1(testInput) should be(2)
+  }
+  "part 1" should "return 660" in {
+    Assignment2.executePart1(actualInput) should be(660)
+  }
+
+  "part 2" should "return 1" in {
+    Assignment2.executePart2(testInput) should be(1)
+  }
+
+  "part 2" should "return 530" in {
+    Assignment2.executePart2(actualInput) should be(530)
+  }
 }
